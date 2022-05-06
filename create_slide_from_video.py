bded9ad629ec711f25ba83cd18075bda99c9dd5c
kato-toranosuke-mbp <toranosuke11041999@gmail.com>
2022-05-07 02:53:55 +0900

    [M] change the hash calcuration algorithm and the argument for time.sleep() to run at regular intervals.

--- create_slide_from_video.py
+++ create_slide_from_video.py
@@ -12,9 +12,10 @@ import queue
 
 # Constants
 OUTPUT_PATH = '/Users/toranosuke/Desktop/screen_shot_path'
-VIDEO_DURATION_MIN = 1
-INTERVAL_SEC = 3
-THSH_DIFF_HASH_VAL = 10
+VIDEO_DURATION_MIN = 5
+# スクショ撮るのに約0.4s, 保存に0.8~0.9s,　全体で約1.5sかかると思った方が良い
+INTERVAL_SEC = 5  # INTERVAL_SEC should be largeer than 1.5s.
+THSH_DIFF_HASH_VAL = 5
 
 # Queue
 img_q = queue.Queue()
@@ -26,6 +27,7 @@ def take_screenshot():
 
     # ビデオ時間以内でスクリーンショットを撮影し続ける
     while (datetime.datetime.now() - video_start_time).seconds <= VIDEO_DURATION_MIN * 60:
+        start_t = time.perf_counter()
         # 撮影
         ss = pyautogui.screenshot()
         # 保存
@@ -35,8 +37,9 @@ def take_screenshot():
         # キューへの登録
         img_q.put(img_path)
 
+        end_t = time.perf_counter()
         print(f'SHOT! {index}')
-        time.sleep(INTERVAL_SEC)
+        time.sleep(INTERVAL_SEC - (end_t - start_t))
 
     # 番兵を挿入
     img_q.put('last')
@@ -44,6 +47,7 @@ def take_screenshot():
 # 画像の類似度により、画像を削除する
 def exclude_similar_imgs():
     last_img_path = -1
+    last_img_hash = -1
     while True:
         if img_q.qsize() > 0:
             img_path = img_q.get()
@@ -55,14 +59,17 @@ def exclude_similar_imgs():
             # 1枚目の画像の時
             if last_img_path == -1:
                 last_img_path = img_path
+                last_img_hash = imagehash.dhash_vertical(
+                    Image.open(last_img_path))
                 continue
 
             # 直前の画像とハッシュ値を比較し、類似画像を削除する
-            last_img_hash = imagehash.average_hash(Image.open(last_img_path))
-            img_hash = imagehash.average_hash(Image.open(img_path))
+            # last_img_hash = imagehash.dhash_vertical(Image.open(last_img_path))
+            img_hash = imagehash.dhash_vertical(Image.open(img_path))
             if abs(img_hash - last_img_hash) > THSH_DIFF_HASH_VAL:
                 # 異なる画像の場合
                 last_img_path = img_path
+                last_img_hash = img_hash
 
                 print(
                     f'[diff img]{img_path}, {last_img_hash}, hash diff: {abs(img_hash - last_img_hash)}')
@@ -76,7 +83,6 @@ def exclude_similar_imgs():
 
 def main():
     future_list = []
-    # with futures.ThreadPoolExecutor(max_workers=4) as executor:
     with futures.ThreadPoolExecutor() as executor:
         future_take_ss = executor.submit(take_screenshot)
         future_list.append(future_take_ss)
