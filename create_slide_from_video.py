0d1973af03a2d9c74abff78b7a09982e399346fb
kato-toranosuke-mbp <toranosuke11041999@gmail.com>
2022-05-07 18:45:25 +0900

    [M] add the function to delete non-target images.

--- create_slide_from_video.py
+++ create_slide_from_video.py
@@ -9,9 +9,11 @@ import time
 from PIL import Image
 import imagehash
 import queue
+import glob
 
 # Constants
 OUTPUT_PATH = '/Users/toranosuke/Desktop/screen_shot_path'
+TARGET_IMGS_PATH = './target_imgs_ds'
 VIDEO_DURATION_MIN = 5
 # スクショ撮るのに約0.4s, 保存に0.8~0.9s,　全体で約1.5sかかると思った方が良い
 INTERVAL_SEC = 5  # INTERVAL_SEC should be largeer than 1.5s.
@@ -46,6 +48,9 @@ def take_screenshot():
 
 # 画像の類似度により、画像を削除する
 def exclude_similar_imgs():
+    # 保存対象画像のハッシュ値を取得
+    target_img_hash = calc_target_imgs_hash(TARGET_IMGS_PATH)
+
     last_img_path = -1
     last_img_hash = -1
     while True:
@@ -63,23 +68,49 @@ def exclude_similar_imgs():
                     Image.open(last_img_path))
                 continue
 
+            # 保存対象の画像でない時
+            if abs(imagehash.colorhash(Image.open(img_path)) - target_img_hash) > THSH_DIFF_HASH_VAL:
+                print(f'[not target img]{img_path}')
+                os.remove(img_path)
+                continue
+
             # 直前の画像とハッシュ値を比較し、類似画像を削除する
-            # last_img_hash = imagehash.dhash_vertical(Image.open(last_img_path))
             img_hash = imagehash.dhash_vertical(Image.open(img_path))
             if abs(img_hash - last_img_hash) > THSH_DIFF_HASH_VAL:
                 # 異なる画像の場合
-                last_img_path = img_path
-                last_img_hash = img_hash
-
                 print(
                     f'[diff img]{img_path}, {last_img_hash}, hash diff: {abs(img_hash - last_img_hash)}')
+                last_img_path = img_path
+                last_img_hash = img_hash
             else:
                 # 類似画像の場合
-                os.remove(img_path)
                 print(
                     f'[similar img]{img_path}, {last_img_path}, hash diff: {abs(img_hash - last_img_hash)}')
+                os.remove(img_path)
+
         time.sleep(INTERVAL_SEC / 2)
 
+def calc_target_imgs_hash(path):
+    file_list = glob.glob(f'{path}/*.*')
+    dec_hashes = []
+
+    img_hash = imagehash.colorhash(Image.open(file_list[0]))
+
+    # for file_path in file_list:
+    #     file_hash = imagehash.colorhash(Image.open(file_path))
+    #     # ハッシュ値を文字列に変換し、10進数にさらに変換する
+    #     dec_hash = int(str(file_hash), 16)
+    #     dec_hashes.append(dec_hash)
+
+    # # 10進数ハッシュ値の平均を求める
+    # dec_hash_mean = int(sum(dec_hashes) / len(dec_hashes))
+    # # 16進数文字列に変換
+    # hex_hash_str = hex(dec_hash_mean)
+    # print(type(hex_hash_str))
+    # # ImageHashに変換
+    # img_hash = imagehash.hex_to_hash(hex_hash_str)
+
+    return img_hash
 
 def main():
     future_list = []
